91802f63e478f8e4ff2affca25b336d8af77ca8d
kclucus7 <kclucus7@gmail.com>
2026-09-23 15:31:19 -0400

    minor changes

--- predict_pure_premium.py
+++ predict_pure_premium.py
@@ -6,19 +6,11 @@ def predict_pure_premium(age_band, area, freq_dict, sev_dict):
     # because the regression was log-linear
     estimated_freqs = {key: np.exp(value) for key, value in freq_dict.items()}
     estimated_sevs = {key: np.exp(value) for key, value in sev_dict.items()}
-    age_key = ''
-    area_key = ''
-    if age_band == '18-21': 
-        age_key = 'intercept' 
-    else: 
-        age_key = age_band
-
-    if area == 'C': 
-        area_key = 'intercept'
-    else: 
-        area_key = 'intercept'
-
-    
+    age_key = 'age_band_' + age_band
+    area_key = 'area_density_type_' + area
+    predicted_freq = estimated_freqs['intercept']*estimated_freqs.get(age_key, 1.0)*estimated_freqs.get(area_key, 1.0)
+    predicted_sev = estimated_sevs['intercept']*estimated_sevs.get(age_key, 1.0)*estimated_sevs.get(area_key, 1.0)
+    return predicted_freq*predicted_sev
 
 
 
